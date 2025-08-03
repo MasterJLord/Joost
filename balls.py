bbdfc39fc07954754d3eb5ff1c7b11e1522afcb6
MasterJLord <jayden.cammarata@gmail.com>
2025-08-02 23:40:43 -0700

    Added functions for moving around as a player

--- balls.py
+++ balls.py
@@ -1,8 +1,7 @@
-import pygame, math
-
+import pygame, math, typing
 
 class ball:
-    def __init__(self, color : pygame.color, radius : int, mass : int, position: tuple, velocity : tuple, acceleration : tuple, drag : int):
+    def __init__(self, color : pygame.color, radius : int, mass : int, position: list, velocity : list, acceleration : list, drag : int):
         self.color = color
         self.radius = radius
         self.mass = mass
@@ -17,9 +16,37 @@ class ball:
         self.velocity[0] = self.acceleration[0] / self.drag + (self.velocity[0] - self.acceleration[0] / self.drag) * math.pow(-1 * self.drag * deltaTime)
         self.velocity[1] = self.acceleration[1] / self.drag + (self.velocity[1] - self.acceleration[1] / self.drag) * math.pow(-1 * self.drag * deltaTime)
 
+    def checkWallCollisions(self, stageWidth : int, deltaTime : int):
+        firstCollision = deltaTime + 1
+
+        # TODO : figure out what to return for the soccer ball
+        if (firstCollision > deltaTime):
+            return None
+        return firstCollision
+
 class playerBall(ball):
-    def __init__(self, color : pygame.color, radius : int, mass : int, position: tuple, velocity : tuple, acceleration : tuple, drag : int):
-        super().__init__(color, radius, mass, position, velocity, acceleration, drag)
+    jumpHeight = 0
+    moveSpeed = 0
+    @staticmethod
+    def setAttributes(jumpHeight : int, moveSpeed : int):
+        playerBall.jumpHeight = jumpHeight
+        playerBall.moveSpeed = moveSpeed
+
+    def moveRight(self):
+        self.acceleration[0] = playerBall.moveSpeed
+
+    def moveLeft(self):
+        self.acceleration[0] = -1 * playerBall.moveSpeed
+
+    def stopMoving(self):
+        self.acceleration[0] = 0
+
+    def jump(self):
+        if self.velocity[1] < 0:
+            self.velocity[1] = playerBall.jumpHeight
+        else:
+            self.velocity[1] += playerBall.jumpHeight
+
 
 class goalBall(ball):
     pass
